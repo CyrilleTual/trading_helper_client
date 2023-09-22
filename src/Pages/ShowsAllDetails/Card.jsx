108ee5e09a7d2f4ff7cd61d9d34499a2c06ae2a8
cyrilletual <cyrille.tual@gmail.com>
2023-09-22 15:00:25 +0200

    review trade ok

--- src/Pages/ShowsAllDetails/Card.jsx
+++ src/Pages/ShowsAllDetails/Card.jsx
@@ -1,9 +1,105 @@
-import React from 'react'
+import styles from "./card.module.css";
+import PerfMeter from "../../Components/PerfMeter/Index";
+import { Loading } from "../../Components/Loading/Index";
 
-function Card() {
+function Card({trade}) {
+   
+ 
+  // metriques du trade en cours
+
+  if (!trade) {
+    return;
+  }
+ 
+  const balance = +(
+    trade.position === "long"
+      ? (+trade.lastQuote - trade.pru) * trade.actualQuantity
+      : (+trade.pru - trade.lastQuote) * trade.actualQuantity
+  ).toFixed(0);
+
+  const balancePc = +(
+    trade.position === "long"
+      ? (trade.lastQuote - trade.pru) / trade.pru
+      : (trade.pru - trade.lastQuote) / trade.pru
+  ).toFixed(2);
+
+  const potential =
+    (trade.position === "long"
+      ? (trade.target - trade.pru) * trade.actualQuantity
+      : (trade.pru - trade.target) * trade.actualQuantity).toFixed(2);
+  const potentialPc =
+   ( trade.position === "long"
+      ? (trade.target - trade.pru) / trade.pru
+      : (trade.pru - trade.target) / trade.pru).toFixed(2);
+  const risk =
+    (trade.position === "long"
+      ? (trade.stop - trade.pru) * trade.actualQuantity
+      : (trade.pru - trade.stop) * trade.actualQuantity).toFixed(2);
+  const riskPc =
+    (trade.position === "long"
+      ? (trade.stop - trade.pru) / trade.pru
+      : (trade.pru - trade.stop) / trade.pru).toFixed(2);
+  const rr = (risk < 0 ? -potential / risk : 0).toFixed(2);
+
+  const targetAtPc = ((trade.target - trade.lastQuote) / trade.lastQuote *100).toFixed(2);
+  const riskAtPc = ((trade.stop - trade.lastQuote) / trade.lastQuote*100).toFixed(2);
+
   return (
-    <div>Card</div>
-  )
+    <>
+      {!trade ? (
+        <Loading />
+      ) : (
+        <main className={styles.cardShow}>
+          <h2 className={styles.title2}>{trade.title}</h2>
+          <p>Portefeuille {trade.portfolio}</p>
+          <p>
+            C'est un trade {trade.position}, le dernier cours est à{" "}
+            {trade.lastQuote} {trade.symbol}.
+          </p>
+          <p>
+            Le PRU est de {trade.pru} {trade.symbol} pour une ligne de{" "}
+            {trade.actualQuantity} titres. <br />
+            Ligne en {balance > 0 ? (
+              <span>gain</span>
+            ) : (
+              <span>perte</span>
+            )} de {balance} {trade.symbol} soit {balancePc.toFixed(2)} % .
+            <br />
+            Actuellement, objectif : {trade.target} {trade.symbol} et stop{" "}
+            {trade.stop} {trade.symbol}
+            <br />
+            Si objectif ralié: {potential} {trade.symbol} soit {potentialPc} %.{" "}
+            <br />
+            Si stop déclanché: {risk} {trade.symbol} soit {riskPc} %.
+            <br />
+          </p>
+          {rr > 0 ? (
+            <p>Risk/reward de {rr}</p>
+          ) : potential < 0 ? (
+            <p>Trade perdant</p>
+          ) : (
+            <p>Trade sans rique</p>
+          )}
+          <p>L'objectifs est à {targetAtPc} % et le stop à {riskAtPc} % .</p>
+
+          <div className={styles.meter_container}>
+            <PerfMeter
+              legend={
+                balance > 0
+                  ? `Gain actuel : ${balance} ${trade.symbol} `
+                  : `Perte actuelle : ${balance} ${trade.symbol} `
+              }
+              min={risk}
+              max={potential}
+              perf={balance}
+              meterWidth={styles.meterWidth}
+              meterHeight={styles.meterHeight}
+            />
+          </div>
+        </main>
+      )}
+    </>
+  );
 }
 
 export default Card
--- src/Pages/ShowsAllDetails/Index.jsx
+++ src/Pages/ShowsAllDetails/Index.jsx
@@ -1,35 +1,33 @@
-import React, { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux';
-import { useGetPortfoliosByUserQuery, useGetTradesActivesByUserQuery } from '../../store/slice/tradeApi';
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useGetTradesActivesByUserQuery } from "../../store/slice/tradeApi";
 
-import { Loading } from '../../Components/Loading/Index';
-import { prepare } from './utils';
-import Card from './Card';
-
- 
- 
+import { Loading } from "../../Components/Loading/Index";
+import { prepare } from "./utils";
+import Card from "./Card";
+import BtnAction from "../../Components/UI/BtnAction";
+import styles from "./index.module.css";
 
 function ShowAllDetails() {
-  
   /// gestion du statut visiteur //////////////////////////////////////
 
   const role = useSelector((state) => state.user.infos.role);
   let id = useSelector((state) => state.user.infos.id);
-  let isVisitor = false ;
+  let isVisitor = false;
 
   if (role.substring(0, 7) === "visitor") {
     id = role.substring(8);
     isVisitor = true;
   }
 
-  ////// preparation des variables utilisées ici 
-  const [show, setShow] = useState ({
+  ////// preparation des variables utilisées ici
+  const [show, setShow] = useState({
     trades: [],
     tradesIds: [],
     portfoliosIds: [],
     selectedTradeId: null,
-  })
-
+    indexOfTradeSelected: 0,
+  });
 
   // Récupère tous les trades ouverts par id d'user
   const {
@@ -39,19 +37,39 @@ function ShowAllDetails() {
     isError: tradesisError1,
   } = useGetTradesActivesByUserQuery(id);
 
-  // peuple les variables utilisées pour passer à la card les infos 
+  // peuple les variables utilisées pour passer à la card les infos /////
   useEffect(() => {
     if (tradesisSuccess) {
       const { trades, tradesIds, portfoliosIds } = prepare(originalsTrades);
-      setShow ( {...show, 
-        trades:trades, 
-        tradesIds:tradesIds,
-        selectedTradeId:tradesIds[0],
-        portfoliosIds:portfoliosIds})
+      setShow({
+        ...show,
+        trades: trades,
+        tradesIds: tradesIds,
+        selectedTradeId: tradesIds[0],
+        portfoliosIds: portfoliosIds,
+      });
     }
   }, [tradesisSuccess]);
 
-  console.log (show.trades)
+  // action sur les boutons previous et next
+  const handleClick = (action) => {
+    if (action === "previous") {
+      if (show.indexOfTradeSelected !== 0) {
+        setShow({
+          ...show,
+          indexOfTradeSelected: show.indexOfTradeSelected - 1,
+        });
+      }
+    }
+    if (action === "next") {
+      if (show.indexOfTradeSelected < show.trades.length - 1) {
+        setShow({
+          ...show,
+          indexOfTradeSelected: show.indexOfTradeSelected + 1,
+        });
+      }
+    }
+  };
 
   return (
     <>
@@ -59,11 +77,32 @@ function ShowAllDetails() {
         <Loading />
       ) : (
         <>
-          <Card/>
+          <h1>Details des trades</h1>
+
+          <Card trade={show.trades[show.indexOfTradeSelected]} />
+          <div className={styles.btn_wrapper}>
+            <span className={styles.btn}>
+              <BtnAction
+                className={styles.btn}
+                value={"Previous"}
+                action={() => handleClick("previous")}
+                name={"previous"}
+              />
+            </span>
+
+            <span className={styles.btn}>
+              <BtnAction
+                className={styles.btnDetail}
+                value={"Next"}
+                action={() => handleClick("next")}
+                name={"next"}
+              />
+            </span>
+          </div>
         </>
       )}
     </>
   );
 }
 
-export default ShowAllDetails
+export default ShowAllDetails;
